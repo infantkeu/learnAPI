7a8f40575300f9034a4049a4ed62b4c3df635574
infantkeu <70271844+infantkeu@users.noreply.github.com>
2020-09-04 17:51:10 +0300

    Получение из API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { moviesData } from "../moviesData";
+// import { moviesData } from "../moviesData";
 import MovieItem from "./MovieItem";
+import { API_URL, API_KEY_3 } from "../utils/api";
 
 // UI = fn(state, props)
 
@@ -11,14 +12,31 @@ class App extends React.Component {
     super();
 
     this.state = {
-      movies: moviesData,
+      movies: [],
       moviesWillWatch: []
     };
   }
 
-  deleteMovie = movie => {
+  componentDidMount() {
+    console.log("didMount");
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}`)
+      .then((response) => {
+        console.log("fetch");
+        return response.json();
+      })
+      .then((data) => {
+        console.log("fetch", data);
+        this.setState({
+          movies: data.results
+        });
+      });
+  }
+
+  deleteMovie = (movie) => {
     console.log(movie.id);
-    const updateMovies = this.state.movies.filter(item => item.id !== movie.id);
+    const updateMovies = this.state.movies.filter(
+      (item) => item.id !== movie.id
+    );
     console.log(updateMovies);
 
     // this.state.movies = updateMovies;
@@ -27,7 +45,7 @@ class App extends React.Component {
     });
   };
 
-  addMovieToWillWatch = movie => {
+  addMovieToWillWatch = (movie) => {
     const updateMoviesWillWatch = [...this.state.moviesWillWatch];
     updateMoviesWillWatch.push(movie);
 
@@ -36,9 +54,9 @@ class App extends React.Component {
     });
   };
 
-  deleteMovieFromWillWatch = movie => {
+  deleteMovieFromWillWatch = (movie) => {
     const updateMoviesWillWatch = this.state.moviesWillWatch.filter(
-      item => item.id !== movie.id
+      (item) => item.id !== movie.id
     );
 
     this.setState({
@@ -53,7 +71,7 @@ class App extends React.Component {
         <div className="row mt-4">
           <div className="col-9">
             <div className="row">
-              {this.state.movies.map(movie => {
+              {this.state.movies.map((movie) => {
                 return (
                   <div className="col-6 mb-4" key={movie.id}>
                     <MovieItem
@@ -70,7 +88,7 @@ class App extends React.Component {
           <div className="col-3">
             <h4>Will Watch: {this.state.moviesWillWatch.length} movies</h4>
             <ul className="list-group">
-              {this.state.moviesWillWatch.map(movie => (
+              {this.state.moviesWillWatch.map((movie) => (
                 <li key={movie.id} className="list-group-item">
                   <div className="d-flex justify-content-between">
                     <p>{movie.title}</p>
